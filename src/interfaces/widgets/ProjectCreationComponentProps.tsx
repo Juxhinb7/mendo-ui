b79335fb932fb1651a6dfea1f36d558544d03d2b
juxhinb7 <juxhinbazelli@gmail.com>
2024-04-07 17:59:46 +0200

    Fixes

--- src/interfaces/widgets/ProjectCreationComponentProps.tsx
+++ src/interfaces/widgets/ProjectCreationComponentProps.tsx
@@ -1,7 +1,6 @@
 interface ProjectCreationComponentProps {
     submitHandler: (event: React.FormEvent<Element>, entry?: {[key: string]: string}) => Promise<void>;
     setTitle: React.Dispatch<React.SetStateAction<string>>;
-    value: string;
     setDescription: React.Dispatch<React.SetStateAction<string>>;
 }
 
