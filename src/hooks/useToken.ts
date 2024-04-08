07b4c8e7d6e7720c813232a35c900e107a9e1228
juxhinb7 <juxhinbazelli@gmail.com>
2024-04-08 18:28:11 +0200

    Fixed

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -1,5 +1,6 @@
 import { atom, useAtom } from "jotai";
 import { useMemo} from "react";
+import { useLocation } from "react-router-dom";
 
 
 
@@ -8,8 +9,9 @@ const useToken = () => {
         const userToken = localStorage.getItem("token");
         return userToken && userToken;
     }
+    const location = useLocation();
     
-    const tokenAtom = useMemo(() => atom(getToken()), []); 
+    const tokenAtom = useMemo(() => atom(getToken()), [location.pathname]); 
     const [token, setToken] = useAtom(tokenAtom);
 
     const saveToken = (userToken?: string) => {
