b6a6b7e3a342746b7e6d72359b5ea38f850b6280
juxhinb7 <juxhinbazelli@gmail.com>
2024-08-17 15:07:30 +0200

    Changed screen breakpoints for Taskboardcontainer

--- src/components/containers/TaskboardContainer.tsx
+++ src/components/containers/TaskboardContainer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react"
 
 const TaskboardContainer = ({children}: PropsWithChildren): JSX.Element => {
     return (
-        <div className="grid 2k:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 gap-5 items-start">
+        <div className="grid 2k:grid-cols-4 2xl:grid-cols-4 md:grid-cols-2 gap-5 items-start">
             {children}
         </div>
     )
